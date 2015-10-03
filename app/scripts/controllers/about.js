2fb54add2f42cee181582968f172d093e832296b
Andrei Railean <andrei@siter.com.au>
2015-10-03 22:18:16 +1000

    using yeoman angular generator to bootstrap the environment

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clockApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the clockApp
+ */
+angular.module('clockApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clockApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clockApp
+ */
+angular.module('clockApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
